1c4abad9ce4c15739717c48094c7595f67aa5429
zaid223r <118609095+zaid223r@users.noreply.github.com>
2024-03-10 00:32:39 +0300

    Add files via upload

---
+++ localhost/MyLocalIPAddress.java
@@ -0,0 +1,14 @@
+package net.work;
+
+import java.net.*;
+
+public class MyLocalIPAddress {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Your IP address is "+ InetAddress.getLocalHost());
+        } catch (java.net.UnknownHostException UkHE) {
+            System.out.println("\n\nCouldn't find the your IP address");
+        
+    }
+}
+}
